2e149f61a854916ac0724ea2e6d34197bacdbb90
TheGrim159 <47199533+TheGrim159@users.noreply.github.com>
2019-03-28 19:26:20 -0400

    Revert "Update Sword.cs"
    This reverts commit db4c116198209218dfb8851f2d28d649d8356556.

--- Assets/Items/Sword.cs
+++ Assets/Items/Sword.cs
@@ -4,19 +4,9 @@ public class Sword : Textbook {
 	
 	public override TimedMethod[] Use() {
 		Attacks.SetAudio("Sword", 15);
-        if (Attacks.EvasionCheck(Party.GetEnemy(), Party.GetPlayer().GetAccuracy())) {
-            return new TimedMethod[] {new TimedMethod(60, "Log", new object[] {
-            Party.GetPlayer().GetName() + " attacked with the sword. It broke immediately"}),
-            new TimedMethod(0, "Audio", new object[] {"Big Swing"}), new TimedMethod(0, "StagnantAttack", new object[] {
-            true, Party.GetPlayer().GetStrength() + 7, Party.GetPlayer().GetStrength() + 11, Party.GetPlayer().GetAccuracy(), true, true, false})};
-        } else
-        {
-            Party.AddItem(this);
-            return new TimedMethod[] {new TimedMethod(60, "Log", new object[] {
-            Party.GetPlayer().GetName() + " attacked with the sword." }) ,
-            new TimedMethod(0, "Audio", new object[] {"Big Swing"}), new TimedMethod(0, "StagnantAttack", new object[] {
-            true, Party.GetPlayer().GetStrength() + 7, Party.GetPlayer().GetStrength() + 11, Party.GetPlayer().GetAccuracy(), true, true, false})};
-        }
-		
+		return new TimedMethod[] {new TimedMethod(60, "Log", new object[] {
+			Party.GetPlayer().GetName() + " attacked with the sword. It broke immediately"}),
+		    new TimedMethod(0, "Audio", new object[] {"Big Swing"}), new TimedMethod(0, "StagnantAttack", new object[] {
+			true, Party.GetPlayer().GetStrength() + 7, Party.GetPlayer().GetStrength() + 11, Party.GetPlayer().GetAccuracy(), true, true, false})};
 	}
 }
